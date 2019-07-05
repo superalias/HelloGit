d05087cb2fea378cd584327840facbba27ba3c0f
superalias <1311445305@qq.com>
2019-07-05 15:54:11 +0800

    添加了！！！

--- src/com/yff/HelloGit.java
+++ src/com/yff/HelloGit.java
@@ -2,6 +2,6 @@ package com.yff;
 
 public class HelloGit {
     public static void main(String[] args) {
-        System.out.println("Hello,Git!");
+        System.out.println("Hello,Git!!!");
     }
 }
